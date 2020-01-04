b0bdcac104d92e9cb5a0cf0ea0025070832fd77a
Marco <ceccottimarco89@gmail.com>
2020-01-04 02:51:29 +0100

    TODO

--- src/app/services/user-session.service.ts
+++ src/app/services/user-session.service.ts
@@ -12,6 +12,7 @@ export class UserSession {
     constructor(private tokenDecoderService: TokenDecoderService,
                 private userService: UserService) {
 
+                    // TODO PER OTTENERE IL CODICE 'ROTTO' COMMENTA TUTTO QUESTO CORPO DEL COSTRUTTORE
                     const tokenInfo = this.tokenDecoderService.getDecodedAccessToken(localStorage.getItem('access_token'));
                     if (tokenInfo != null) {
                         this.userService.userInfo(tokenInfo.sub)
